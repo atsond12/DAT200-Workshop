ba013eab89b48381ccf2e5bca0587490ad17320c
atsond12 <atsond12@student.uia.no>
2024-10-24 13:58:15 +0200

    New Objects

--- ThreeJS/basicWorld.js
+++ ThreeJS/basicWorld.js
@@ -25,7 +25,7 @@ function windowResize() {
   world.renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-export function createWorld() {
+export function createWorld(createObjectsFunction) {
   const groundPlaneSize = 150; 
   const lightColor = "#ffffff";
   const lightIntensity = 1;
@@ -33,14 +33,22 @@ export function createWorld() {
   world.scene = new THREE.Scene();
   world.scene.background = new THREE.Color(backgroundColor);
   world.scene.fog = new THREE.Fog(backgroundColor, groundPlaneSize/5, groundPlaneSize/2);
+  
   /* Here is a nice place to add lights */
   let light = new THREE.AmbientLight(lightColor, lightIntensity);
   world.scene.add(light);
 
+  light = new THREE.DirectionalLight(lightColor, lightIntensity/2);
+  light.position.set(5, 5, 5);
+  light.castShadow = true;
+  light.bias = -0.001;
+  light.shadow.mapSize.set(2048, 2048);
+  light.shadow.radius = 5;
+  world.scene.add(light);
 
   /* Create a ground plane */
   let geo = new THREE.PlaneGeometry(groundPlaneSize, groundPlaneSize);
-  let mat = new THREE.MeshPhongMaterial({ color: "#a6a6a6", depthWrite: false, shininess: 200});
+  let mat = new THREE.MeshPhongMaterial({ color: "#a6a6a6", depthWrite: true, shininess: 200});
   let mesh = new THREE.Mesh(geo, mat);
   mesh.rotation.x = -90 * deg;
   mesh.receiveShadow = true;
@@ -51,10 +59,13 @@ export function createWorld() {
   world.scene.add(helper);
 
   /* Here is a nice place to add objects */
+  if(createObjectsFunction){
+    createObjectsFunction(world.scene);
+  }
 
   /* Create a camera */
   world.camera = new THREE.PerspectiveCamera(45, getAspectRatio(), 0.001, groundPlaneSize * 2);
-  world.camera.position.set(5, 2, -5);
+  world.camera.position.set(7, 3, -7);
   world.camera.lookAt(0, 0, 0);
 
   /* Create a renderer */
